1cfc380cd2af9b0ea0c3d273c47030b78c2f0e2b
js-jslog <josephsinfield@yahoo.com>
2017-08-06 18:53:24 +0100

    creating sync file read example

--- app/index.js
+++ app/index.js
@@ -1,15 +1,10 @@
 "use strict"
-const calc = require("./calc");
-const readline = require("readline");
+const fs = require("fs");
+let content;
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-rl.question("Enter a number", (a1) => {
-    rl.question("Enter another number", (a2) => {
-        const result = calc.sum([a1, a2,]);
-        console.log(`The result is: ${result}`);
-    });
-});
+try {
+    content = fs.readFileSync("file.md", "utf-8");
+} catch (ex) {
+    console.log(ex);
+}
+console.log(content);
