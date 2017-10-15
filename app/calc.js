a51b3321cacb58f8bab19509c5889a86d34fd384
js-jslog <josephsinfield@yahoo.com>
2017-10-15 03:00:11 +0100

    using express-handlebars

--- app/calc.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-var sum = function (arr) {
-    return arr.reduce(function(a, b) {
-        return a + b;
-    }, 0);
-};
-
-module.exports.sum = sum;
--- app/file_read.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-const fs = require("fs");
-const readfileCallback = function (err, content) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(content)
-};
-fs.readFile("file.md", "utf-8", readfileCallback);
