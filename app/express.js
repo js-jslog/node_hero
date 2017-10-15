772643ed07cbf210a1687e2a66d92d6041d09085
js-jslog <josephsinfield@yahoo.com>
2017-10-15 02:44:48 +0100

    more complex express example

--- app/express.js
+++ app/express.js
@@ -2,16 +2,30 @@
 
 const express = require("express");
 const app = express();
-const port = 3000;
+
+app.use((request, response, next) => {
+    console.log(request.headers);
+    next();
+});
+
+app.use((request, response, next) => {
+    request.chance = Math.random();
+    next();
+});
+
+app.use((err, request, response, next) => {
+    console.log(err);
+    response.status(500).send("something broke");
+});
 
 app.get("/", (request, response) => {
-    response.send("Hello from Express!");
+    response.json({
+        chance: request.chance,
+    });
 });
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log("something bad happened", err);
-    }
-
-    console.log(`server is listening on ${port}`);
+app.get("/error", (request, response) => {
+    throw new Error("oops");
 });
+
+app.listen(3000);
